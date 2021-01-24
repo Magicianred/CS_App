911a671661eb9ec02867dec71c6572203644584e
Sinan <sinanfen@hotmail.com>
2021-01-24 22:07:04 +0300

    Add files via upload

---
+++ CsApp/CsApp/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsApp
+{
+    class Product
+    {
+        public string ProductName { get; set; }
+        public string ProductInfo { get; set; }
+        public int StockInProcess { get; set; }
+
+
+    }
+}
---
+++ CsApp/CsApp/ProductForWhileForeach.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsApp
+{
+    class ProductForWhileForeach
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.ProductName = "Apple";
+            product1.ProductInfo = "Fruit";
+            product1.StockInProcess = 50;
+
+            Product product2 = new Product();
+            product2.ProductName = "Pear";
+            product2.ProductInfo = "Fruit";
+            product2.StockInProcess = 50;
+
+            Product product3 = new Product();
+            product3.ProductName = "Banana";
+            product3.ProductInfo = "Fruit";
+            product3.StockInProcess = 49;
+
+            Product[] products = new Product[] { product1, product2, product3 };
+
+            Console.WriteLine("- - - USED FOR LOOP - - -");
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine("Product Name: " + products[i].ProductName + " --Product Info: " + products[i].ProductInfo + " --Stock In Process: " + products[i].StockInProcess);
+            }
+
+            Console.WriteLine("- - - USED WHILE LOOP - - -");
+            int x = 0;
+            while (x<products.Length)
+            {
+                Console.WriteLine("Product Name: " + products[x].ProductName + " --Product Info: " + products[x].ProductInfo + " --Stock In Process: " + products[x].StockInProcess);
+                x++;
+            }
+
+            Console.WriteLine("- - - USED FOREACH LOOP - - -");
+            foreach (var k in products)
+            {
+                Console.WriteLine("Product Name: " + k.ProductName+ " --Product Info: " + k.ProductInfo+ " --Stock In Process: " + k.StockInProcess);
+            }
+
+        }
+    }
+}
